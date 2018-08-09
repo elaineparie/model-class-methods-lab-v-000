be8ddb9950cf7744be95cb5793f46e0f7dff038b
Elaine Anderson <eparie@bu.edu>
2018-08-09 22:00:43 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -18,6 +18,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-    self.joins(boats: :classifications).where('classifications.name' != "Sailboat")
+    where.not('classifications.name' != "Sailboat")
   end
 end
